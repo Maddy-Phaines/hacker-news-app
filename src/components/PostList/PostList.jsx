3ef19ea857f49d949c5a304d395ece31193be593
Maddy-Phaines <maddy.phaines@gmail.com>
2025-06-03 21:02:45 +0100

    Implement PostList and PostItem components to render posts

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,2 +1,15 @@
 /* Receives array of posts, renders list */
 /* Reusable for homepage & search */
+import PostItem from "./PostItem";
+
+const PostList = ({ posts }) => {
+  return (
+    <div>
+      {posts.map((post) => (
+        <PostItem key={post.objectID} post={post} />
+      ))}
+    </div>
+  );
+};
+
+export default PostList;
