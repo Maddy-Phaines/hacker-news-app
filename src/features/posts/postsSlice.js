eba6aa10db5e1bad43be4381c83c778e79834eba
Maddy-Phaines <maddy.phaines@gmail.com>
2025-06-03 18:50:55 +0100

    Implement postsSlice with createAsyncThunk to fetch front page posts

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,41 @@
+/* PostsSlice handles async fetch with createAsyncThunk*/
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const response = await fetch(
+    "https://hn.algolia.com/api/v1/search?tags=front_page"
+  );
+
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  const data = await response.json();
+  return data.hits;
+});
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState: {
+    items: [],
+    status: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPosts.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.items = action.payload;
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default postsSlice.reducer;
