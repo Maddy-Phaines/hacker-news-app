9d6585c2a908295a8be078e34c03d1cb56d85fff
Maddy-Phaines <maddy.phaines@gmail.com>
2025-06-04 20:07:41 +0100

    Register trendingPostsReducer in Redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import postsReducer from "../features/posts/postsSlice";
+import trendingPostsReducer from "../features/trendingPosts/trendingPostsSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    posts: postsReducer,
+    trendingPosts: trendingPostsReducer,
+  },
 });
