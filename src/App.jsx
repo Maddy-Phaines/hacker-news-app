e5b344face42035271d41e48fe8df4ce11c5b9e2
Maddy-Phaines <maddy.phaines@gmail.com>
2025-07-10 17:12:37 +0100

    chore(ui): remove unneeded Route from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import SearchResultsPage from "./pages/SearchResultsPage";
 import PostDetailPage from "./pages/PostDetailPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import SearchBarPage from "./components/SearchBarPage";
 
 function App() {
   return (
-    <div className="min-h-screen bg-[var(--color-bg)] text-[var(--color-text)] transition-colors">
+    <div
+      className="min-h-screen bg-[var(--color-bg)] 
+    text-[var(--color-text)] transition-colors"
+    >
       <BrowserRouter>
-        <Header />
         <main className="max-w-[1336px] mx-auto bg-[var(--color-bg)]">
           <Routes>
             <Route path="/posts/:id" element={<PostDetailPage />} />
             <Route path="/" element={<HomePage />} />
             <Route path="/search" element={<SearchResultsPage />} />
+            <Route path="/searchpage" element={<SearchBarPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </main>
