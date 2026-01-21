94ad5df8a264492242d4c12f99197d9226374a04
Maddy <maddyphaines.dev@gmail.com>
2026-01-21 19:54:34 +0000

    chore: simplify app root providers

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import { store } from "./app/store";
 import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import * as Tooltip from "@radix-ui/react-tooltip";
 
 // 1) Create a client
 const queryClient = new QueryClient({
@@ -18,13 +17,9 @@ const queryClient = new QueryClient({
 });
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <Tooltip.Provider delayDuration={300}>
-          <App />
-        </Tooltip.Provider>
-      </QueryClientProvider>
-    </Provider>
-  </StrictMode>
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </Provider>,
 );
